21006ac37440ad18266c6eac15ddc2bde8b94fdb
Gabriel Santo <gvgesanto2@gmail.com>
2022-10-10 16:36:57 -0300

    feat: Add the Devise library for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :recipes
   resources :foods
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
 end
