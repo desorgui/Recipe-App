1469be2e51c8a2ae0fc2e2da5460d99c02d43d8d
Gabriel Santo <gvgesanto2@gmail.com>
2022-10-13 19:45:11 -0300

    feat: Style the new recipe form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,10 +8,14 @@ class Recipe < ApplicationRecord
   scope :by_user, ->(user) { where(user:) }
 
   validate :image_type
+  validates :preparation_time, numericality: { only_float: true, greater_than_or_equal_to: 0 }
+  validates :cooking_time, numericality: { only_float: true, greater_than_or_equal_to: 0 }
 
   private
 
   def image_type
-    errors.add(:image, "Error: Please upload an image with one of the following extensions: jpeg, jpg, png, or webp!") unless image.content_type.in?(%w[image/png image/jpeg image/jpg image/webp])
+    if image.attached?
+      errors.add(:image, "Error: Please upload an image with one of the following extensions: jpeg, jpg, png, or webp!") unless image.content_type.in?(%w[image/png image/jpeg image/jpg image/webp])
+    end
   end
 end
