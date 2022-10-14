7782be21504539961b60afcd1fc859ec24e8bf5d
Gabriel Santo <gvgesanto2@gmail.com>
2022-10-14 05:05:09 -0300

    feat: Create the show recipe page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,7 +7,9 @@ class RecipesController < ApplicationController
   end
 
   # GET /recipes/1 or /recipes/1.json
-  def show; end
+  def show
+    @foods = Food.all
+  end
 
   # GET /recipes/new
   def new
