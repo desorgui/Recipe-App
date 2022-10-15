01f5fbf7a5a05d3452cdc95e2624aec430e6a4ae
Desor Guishny <desorguishny@gmail.com>
2022-10-15 15:35:48 -0400

    Implement shopping list

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,19 +1,26 @@
 class ShoppingListController < ApplicationController
-  def inventory_food_quantity(food_id)
-    inventory_food = InventoryFood.where(food_id:, inventory_id: params[:id])
-    inventory_food.quantity
+  def inventory_food_quantity(food_id)    
+    inventory_id = 15
+    @inventory_food = InventoryFood.where(food_id:food_id, inventory_id:inventory_id)
+    @inventory_food[0].quantity
   end
 
-  # method to create shopping list
-
   def index
-    @recipe_foods = RecipeFood.where(id: params[:recipe_id])
+    recipe_id = 1
+    inventory_id = 15
+    @recipe_foods = RecipeFood.where(recipe_id:recipe_id)
     @recipe_foods.each do |recipe_food|
-      if recipe_food.quantity > inventory_food_quantity(recipe_food.food_id)
-        @difference = recipe_food.quantity - inventory_food_quantity(recipe_food.food_id)
+      @shopping_list ||= []       
+      food_to_find = Food.find(recipe_food.food_id)
+      if InventoryFood.where(food_id: recipe_food.food_id, inventory_id:inventory_id).blank?
+        @shopping_list << {food: food_to_find, quantity: recipe_food.quantity}
+      else
+        if recipe_food.quantity > inventory_food_quantity(recipe_food.food_id)
+          @difference = recipe_food.quantity - inventory_food_quantity(recipe_food.food_id)
+          # add food to shopping list array
+          @shopping_list << {food: food_to_find, quantity:@difference}
+        end
       end
-      # add food to shopping list array
     end
-    @shopping_list << recipe_food.food_id
   end
 end
